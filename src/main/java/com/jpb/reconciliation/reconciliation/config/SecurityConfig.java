0e8ae75772b0a07435b76ad88f3465e0f1d36127
akshayramaniprakash <akshayramani39@gmail.com>
2026-03-19 21:18:51 +0530

    secity changes

--- src/main/java/com/jpb/reconciliation/reconciliation/config/SecurityConfig.java
+++ src/main/java/com/jpb/reconciliation/reconciliation/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
 		configuration.setAllowCredentials(true);
 		configuration.addAllowedOrigin("http://localhost:5173");
 		configuration.addAllowedOrigin("https://jpbreconsit.jiopaymentsbank.com:8080");
-		configuration.addAllowedOrigin("http://16.171.41.19/");  // sit
+		configuration.addAllowedOrigin("http://13.48.46.135/");  // sit
 		configuration.addAllowedOrigin("http://10.142.12.140:8080"); // PROD
 		configuration.addAllowedOrigin("http://192.168.1.103:8081");
 		configuration.addAllowedOrigin("https://jio-recon.jiopaymentsbank.com:8080");
